ecf96d0c65bc8011bc2af937a61332b401b951ef
Ruchi Mandliya <rmandliya18@gmail.com>
2023-07-20 23:33:29 +0530

    add all functionality

--- script.js
+++ script.js
@@ -1,45 +1,106 @@
+// const displayTimer = document.getElementById("timer");
+// const startButton = document.getElementById("start");
+// const stopButton = document.getElementById("stop");
+// const resetButton = document.getElementById("reset");
+
+// let msecs = 0;
+// let secs = 0;
+// let minutes = 0;
+
+// let timerStarted = false;
+
+// startButton.addEventListener("click", function () {
+//   if ((timerStarted = true)) {
+//     clearInterval(timerStarted);
+//     startButton.disabled = false;
+//     stopButton.disabled = true;
+//   }
+//   timerStarted = setInterval(startTimer, 10);
+//   startButton.disabled = true;
+//   stopButton.disabled = false;
+// });
+
+// stopButton.addEventListener("click", function () {
+//   clearInterval(timerStarted);
+//   startButton.disabled = false;
+//   stopButton.disabled = true;
+// });
+
+// resetButton.addEventListener("click", function () {
+//   clearInterval(timerStarted);
+//   displayTimer.innerHTML = `00 : 00 : 00`;
+//   msecs = secs = minutes = 00;
+// });
+
+// function startTimer() {
+//   msecs++;
+//   if (msecs == 100) {
+//     msecs = 0;
+//     secs++;
+//     if (secs == 60) {
+//       secs = 0;
+//       minutes++;
+//     }
+//   }
+
+//   let msecString = msecs < 10 ? `0${msecs}` : msecs;
+//   let secsString = secs < 10 ? `0${secs}` : secs;
+//   let minsString = minutes < 10 ? `0${minutes}` : minutes;
+
+//   displayTimer.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+// }
+
 const displayTimer = document.getElementById("timer");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
 
-let msecs = 0;
-let secs = 0;
-let minutes = 0;
-
-let timerStarted = false;
-
-startButton.addEventListener("click", function () {
-  if ((timerStarted = true)) {
-    clearInterval(timerStarted);
-  }
-  timerStarted = setInterval(startTimer, 10);
-});
-
-stopButton.addEventListener("click", function () {
-  clearInterval(timerStarted);
-});
-
-resetButton.addEventListener("click", function () {
-  clearInterval(timerStarted);
-  displayTimer.innerHTML = `00 : 00 : 00`;
-  msecs = secs = minutes = 00;
-});
+let startTime = 0;
+let passedTime = 0;
+let runningTime;
 
 function startTimer() {
-  msecs++;
-  if (msecs == 100) {
-    msecs = 0;
-    secs++;
-    if (secs == 60) {
-      secs = 0;
-      minutes++;
-    }
-  }
+  startTime = Date.now() - passedTime;
 
-  let msecString = msecs < 10 ? `0${msecs}` : msecs;
-  let secsString = secs < 10 ? `0${secs}` : secs;
-  let minsString = minutes < 10 ? `0${minutes}` : minutes;
+  runningTime = setInterval(() => {
+    passedTime = Date.now() - startTime;
+    displayTimer.textContent = formatTime(passedTime);
+  }, 10);
 
-  displayTimer.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+  startButton.disabled = true;
+  stopButton.disabled = false;
 }
+
+function formatTime(passedTime) {
+  const milliseconds = Math.floor((passedTime % 1000) / 10);
+  const seconds = Math.floor((passedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((passedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(passedTime / (1000 * 60 * 60));
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+}
+function stopTimer() {
+  clearInterval(runningTime);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+function resetTimer() {
+  clearInterval(runningTime);
+
+  passedTime = 0;
+  displayTimer.textContent = "00:00:00";
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
